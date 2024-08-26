4a96197c92c1cdd41607a566f0880a29e727faeb
AlexanderSlavchev <166736509+AlexanderSlavchev@users.noreply.github.com>
2024-08-26 21:12:27 +0300

    Update ContestType.java

--- src/main/java/com/example/photocontest/models/ContestType.java
+++ src/main/java/com/example/photocontest/models/ContestType.java
@@ -1,7 +1,16 @@
 package com.example.photocontest.models;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "contest_types")
 public class ContestType {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String type;
