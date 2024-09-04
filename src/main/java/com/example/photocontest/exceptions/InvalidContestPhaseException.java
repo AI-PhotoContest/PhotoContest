87f071fd0a8a7bd661958ee7395f01c4cda0de7e
IvanPuhniev <ivan.puhniev.a60@learn.telerikacademy.com>
2024-09-04 14:29:58 +0300

    exception for invalid contest phase

---
+++ src/main/java/com/example/photocontest/exceptions/InvalidContestPhaseException.java
@@ -0,0 +1,8 @@
+package com.example.photocontest.exceptions;
+
+public class InvalidContestPhaseException extends RuntimeException {
+
+    public InvalidContestPhaseException(String message) {
+        super(message);
+    }
+}
