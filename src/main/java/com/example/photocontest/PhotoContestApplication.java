589f0147ec52d41dc6242328d4887eb24e5a5567
AlexanderSlavchev <166736509+AlexanderSlavchev@users.noreply.github.com>
2024-11-03 15:12:38 +0200

    Load sensitive configurations from .env file and keep general settings in application.properties

--- src/main/java/com/example/photocontest/PhotoContestApplication.java
+++ src/main/java/com/example/photocontest/PhotoContestApplication.java
@@ -1,13 +1,18 @@
 package com.example.photocontest;
 
 import com.example.photocontest.services.EmailService;
+import io.github.cdimascio.dotenv.Dotenv;
+import com.example.photocontest.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class PhotoContestApplication {
 
     public static void main(String[] args) {
+        EnvConfig.init();
         SpringApplication.run(PhotoContestApplication.class, args);
     }
 
---
+++ src/main/java/com/example/photocontest/config/EnvConfig.java
@@ -0,0 +1,15 @@
+package com.example.photocontest.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class EnvConfig {
+
+    public static void init() {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+}
